c44c51d5aa95f1d182dd3435fe8e408490f8b63f
Ganesh <123050867+Ganeshfullstock@users.noreply.github.com>
2023-06-21 09:26:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   const [page, setPage] = useState("cart");
   return (
     <div className="App">
+    <h1>Food Order Site</h1>
+      <h3>Your's ordered items is shows bottom of the screen.</h3>
       <section className="cart py-5">
       {page === "cart" && <Cart />}
       </section>
